020e33c3b4d1943f9b658ebe70e70fb7b8cd7a66
Genar Trias Ortiz <genar@acs.li>
2015-11-19 20:48:08 +0100

    removed useless code

--- petfinder.js
+++ petfinder.js
@@ -1,5 +1,5 @@
 Meteor.methods({
-  addPet: function (name) {
+  addPet: function (name, description) {
     // Make sure the user is logged in before inserting a task
     if (! Meteor.userId()) {
       throw new Meteor.Error("not-authorized");
