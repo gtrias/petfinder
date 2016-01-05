c131e57d4fa17e02b28374feb13c90f02643f4c8
Genar Trias Ortiz <genar@acs.li>
2016-01-05 11:24:28 +0100

    better mobile building process

--- build-android.sh
+++ build-android.sh
@@ -2,5 +2,9 @@
 
 meteor build ../petfinder-build --server https://petfinder.casa.genar.me
 cd ../petfinder-build/android/
-jarsigner -digestalg SHA1 release-unsigned.apk petfinder
+jarsigner -digestalg SHA1 -storepass:env JARSIGNER_STOREPASS -keypass:env JARSIGNER_STOREPASS release-unsigned.apk petfinder
+
+rm -rf release-signed.apk
 ~/Android/Sdk/build-tools/22.0.1/zipalign 4 release-unsigned.apk release-signed.apk
+
+echo "" | mutt -a release-signed.apk -s "Here is the new petfinder version" -- gtrias@gmail.com
---
+++ mobile-config.js
@@ -0,0 +1,22 @@
+// This section sets up some basic app metadata,
+// the entire section is optional.
+App.info({
+    id: 'petfinder',
+    version: '0.0.1',
+    name: 'Petfinder',
+    description: 'Petfinder',
+    author: 'Genar Trias',
+    email: 'genar@acs.li',
+    website: 'https://genar.me'
+});
+
+// Set PhoneGap/Cordova preferences
+App.setPreference('HideKeyboardFormAccessoryBar', true);
+App.setPreference('StatusBarOverlaysWebView', false);
+App.setPreference('StatusBarStyle', 'lightcontent');
+App.setPreference('StatusBarBackgroundColor', '#000000');
+App.setPreference('ShowSplashScreenSpinner', false);
+App.setPreference('android-targetSdkVersion', '22');
+App.setPreference('android-minSdkVersion', '19');
+App.accessRule('*');
+
