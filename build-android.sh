d5c659541159417016f1496e6d036087fadd2147
Genar Trias Ortiz <genar@acs.li>
2016-01-10 22:30:18 +0100

    including docker to build process and removing duplicated build routing

--- build-android.sh
+++ build-android.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-meteor build ../mobile-build --server https://petfinder.casa.genar.me
-cd ../mobile-build/android/
+cd ../petfinder-build/android/
 jarsigner -digestalg SHA1 -storepass:env JARSIGNER_STOREPASS -keypass:env JARSIGNER_STOREPASS release-unsigned.apk petfinder
 
 rm -rf release-signed.apk
 ~/Android/Sdk/build-tools/22.0.1/zipalign 4 release-unsigned.apk release-signed.apk
 
 echo "" | mutt -a release-signed.apk -s "Here is the new petfinder version" -- gtrias@gmail.com
+
+docker rm -f petfinder
+
+docker run --restart=always --name petfinder -d -e VIRTUAL_HOST=petfinder.casa.genar.me -e ROOT_URL=http://petfinder.casa.genar.me  --link mongodb  -e MONGO_URL=mongodb://mongodb:27017/petfinder     -v /home/genar/src/meteorjs/petfinder-build:/bundle   -P     meteorhacks/meteord:base
