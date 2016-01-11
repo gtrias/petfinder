1c063462502bbfd43c35caeee0183067a72a9c50
Genar Trias Ortiz <genar@acs.li>
2016-01-11 10:38:36 +0100

    fixing build process

--- build-android.sh
+++ build-android.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 cd ../petfinder-build/android/
+
 jarsigner -digestalg SHA1 -storepass:env JARSIGNER_STOREPASS -keypass:env JARSIGNER_STOREPASS release-unsigned.apk petfinder
 
 rm -rf release-signed.apk
 ~/Android/Sdk/build-tools/22.0.1/zipalign 4 release-unsigned.apk release-signed.apk
 
 echo "" | mutt -a release-signed.apk -s "Here is the new petfinder version" -- gtrias@gmail.com
+echo "" | mutt -a petfinder-signed.apk -s "Here is the new petfinder version" -- gtrias@gmail.com
 
 docker rm -f petfinder
 
